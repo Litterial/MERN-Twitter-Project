f6007c123f034b19f627eb3bb561a54b18c9123c
Litterial <yungice12@gmail.com>
2019-04-25 07:38:53 -0500

    real final push

--- client/src/Components/Banner.js
+++ client/src/Components/Banner.js
@@ -177,10 +177,10 @@ export default class Banner extends Component{
                                     <Button variant='light' size='lg' type='submit'>Submit</Button>
                                 </div>
                             </Form>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            {/*<NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>*/}
+                            {/*<NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>*/}
+                            {/*<NavDropdown.Divider />*/}
+                            {/*<NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>*/}
                         </NavDropdown>
                     </Navbar.Collapse>
                 </Navbar>
